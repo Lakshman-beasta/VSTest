eedbb0afa2f483fd8f87dfb4f35d31131ae99f1c
Lakshman-beasta <lakshman.beasta9@gmail.com>
2019-11-18 03:16:23 +0545

    change into helloworld

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("hi");
-            Console.WriteLine("hi");
+         Console.WriteLine("hello world");
+
         }
     }
 }
