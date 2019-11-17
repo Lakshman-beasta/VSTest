21690686a7ee56f1ed68041b989d705bfbe77acf
Lakshman-beasta <lakshman.beasta9@gmail.com>
2019-11-18 03:08:15 +0545

    add ok

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World laxman!");
+            Console.WriteLine("Hello World laxman ok !");
         }
     }
 }
